8c3f3ab24da1d89c627332ec79f3b7180d91d3e2
lkong888 <105298439+lkong888@users.noreply.github.com>
2023-08-24 16:01:44 +0100

    Delete code directory

--- code/featurecounts2tpm.r
+++
@@ -1,6 +0,0 @@
-a=read.delim(file("stdin"),skip=1)
-a[,7] =a[,7]/a$Length *10**3/sum(a[,7]/a$Length *10**3) *10**6
-write.table(a,stdout(),sep='\t',quote=F,row.names=F)
-
-#rpk=a[,7]/a$Length *10**3
-#TPM=rpk/sum(rpk) *10**6
